b3292b4594db4a925ef7935bffb27d3514400d38
Hoa Nguyen <hoanguyen@ucdavis.edu>
2023-02-18 16:12:37 -0800

    Style fix for hazardnoncombin file
    Signed-off-by: Hoa Nguyen <hoanguyen@ucdavis.edu>

--- src/main/scala/components/hazardnoncombin.scala
+++ src/main/scala/components/hazardnoncombin.scala
@@ -7,19 +7,19 @@ import chisel3._
 /**
  * The hazard detection unit
  *
- * Input:  rs1, the first source register number
- * Input:  rs2, the second source register number
- * Input:  idex_memread, true if the instruction in the ID/EX register is going to read from memory
- * Input:  idex_rd, the register number of the destination register for the instruction in the ID/EX register
+ * Input:  rs1, the first source register number.
+ * Input:  rs2, the second source register number.
+ * Input:  idex_memread, true if the instruction in the ID/EX register is going to read from memory.
+ * Input:  idex_rd, the register number of the destination register for the instruction in the ID/EX register.
  * Input:  exmem_taken, if true, then we are using the nextpc in the EX/MEM register, *not* pc+4.
  * Input:  exmem_meminst, if true, the instruction at MEM stage is a memory instruction.
- * Input:  imem_ready, if true, then the Instruction Memory is ready for another instruction
- * Input:  imem_good, if true, then the **correct** instruction was successfully retrieved
- *                    i.e., if this signal is true, the fetched instruction can safely enter the pipeline
- * Input:  dmem_good, if true, then can unstall CPU for data memory
+ * Input:  imem_ready, if true, then the Instruction Memory is ready for another instruction.
+ * Input:  imem_good, if true, then the **correct** instruction was successfully retrieved,
+ *                    i.e., if this signal is true, the fetched instruction can safely enter the pipeline.
+ * Input:  dmem_good, if true, then can unstall CPU for data memory.
  *
- * Output: pcfromtaken, if true, use the pc from MEM
- * Output: pcstall, if true, stall the pipeline
+ * Output: pcfromtaken, if true, use the pc from MEM.
+ * Output: pcstall, if true, stall the pipeline.
  * Output: if_id_stall, if true, stall the if_id register. 
  * Output: if_id_flush, if true, flush the if_id register. 
  * Output: id_ex_stall, if true, stall the id_ex register. 
@@ -28,10 +28,10 @@ import chisel3._
  * Output: ex_mem_flush, if true, flush the ex_mem register. 
  * Output: mem_wb_stall, if true, stall the mem_wb register. 
  * Output: mem_wb_flush, if true, flush the mem_wb register. 
- * Output: imem_valid, if true, imem can process new instruction
+ * Output: imem_valid, if true, imem can process new instruction.
  *
  * For more information, see Section 4.7 and beginning of 4.8 of Patterson and Hennessy
- * This follows the "Data hazards and stalls" section and the "Assume branch not taken" section
+ * This loosely follows the "Data hazards and stalls" section and the "Assume branch not taken" section
  */
 class HazardUnitNonCombin extends Module {
   val io = IO(new Bundle {
