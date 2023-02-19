01e8142f8e8d853acb8d66f486215fa95423a970
Hoa Nguyen <hoanguyen@ucdavis.edu>
2023-02-18 23:35:28 -0800

    Add a new way to simulate stuff
    Signed-off-by: Hoa Nguyen <hoanguyen@ucdavis.edu>

--- src/main/scala/top.scala
+++ src/main/scala/top.scala
@@ -11,8 +11,10 @@ class Top(val conf: CPUConfig) extends Module
 
   io.success := DontCare
 
+  val system_name = conf.getSystemName()
+
   val cpu  = Module(conf.getCPU())
-  //val l1_cache = Module(conf.getL1Cache(1))
+  val l1_cache = if (system_name == "default") conf.getEmptyCache() else Module(conf.getL1Cache(1))
   val mem  = Module(conf.getNewMem())
 
   val inst_bridge = Module(conf.getIMemBridge())
@@ -23,27 +25,51 @@ class Top(val conf: CPUConfig) extends Module
   cpu.io.imem <> inst_bridge.io.cpu_side_io
   cpu.io.dmem <> data_bridge.io.cpu_side_io
 
+  system_name match {
+    case "default" => build_default_system()
+    case "system1" => build_system_1()
+    case "system2" => build_system_2()
+    case "system3" => build_system_3()
+    case "system4" => build_system_4()
+    case _ => throw new IllegalArgumentException(
+      "Unknown system name. Choices: default, system1, system2, system3, system4."
+    )
+  }
+
+  def build_default_system(): Unit = {
+    mem.wireToInstBridge(inst_bridge)
+    mem.wireToDataBridge(data_bridge)
+  }
+
   // no cache
-  //l1_cache.notUsingInstCache()
-  //l1_cache.notUsingDataCache()
-  //mem.wireToInstBridge(inst_bridge)
-  //mem.wireToDataBridge(data_bridge)
+  def build_system_1(): Unit = {
+    l1_cache.notUsingInstCache()
+    l1_cache.notUsingDataCache()
+    mem.wireToInstBridge(inst_bridge)
+    mem.wireToDataBridge(data_bridge)
+  }
 
   // inst cache only
-  //l1_cache.wireToInstBridge(inst_bridge)
-  //l1_cache.notUsingDataCache()
-  //mem.wireToDataBridge(data_bridge)
-  //mem.wireInstPortsToHigherLevelMemory(l1_cache.io.inst_active_port_pair)
+  def build_system_2(): Unit = {
+    l1_cache.wireToInstBridge(inst_bridge)
+    l1_cache.notUsingDataCache()
+    mem.wireToDataBridge(data_bridge)
+    mem.wireInstPortsToHigherLevelMemory(l1_cache.io.inst_active_port_pair)
+  }
 
   // data cache only
-  //l1_cache.notUsingInstCache()
-  //l1_cache.wireToDataBridge(data_bridge)
-  //mem.wireToInstBridge(inst_bridge)
-  //mem.wireDataPortsToHigherLevelMemory(l1_cache.io.data_active_port_pair)
+  def build_system_3(): Unit = {
+    l1_cache.notUsingInstCache()
+    l1_cache.wireToDataBridge(data_bridge)
+    mem.wireToInstBridge(inst_bridge)
+    mem.wireDataPortsToHigherLevelMemory(l1_cache.io.data_active_port_pair)
+  }
 
   // both data and inst caches
-  //l1_cache.wireToInstBridge(inst_bridge)
-  //l1_cache.wireToDataBridge(data_bridge)
-  //mem.wireInstPortsToHigherLevelMemory(l1_cache.io.inst_active_port_pair)
-  //mem.wireDataPortsToHigherLevelMemory(l1_cache.io.data_active_port_pair)
+  def build_system_4(): Unit = {
+    l1_cache.wireToInstBridge(inst_bridge)
+    l1_cache.wireToDataBridge(data_bridge)
+    mem.wireInstPortsToHigherLevelMemory(l1_cache.io.inst_active_port_pair)
+    mem.wireDataPortsToHigherLevelMemory(l1_cache.io.data_active_port_pair)
+  }
 }
